461da77bf5a250f54e067d431e1637d148bec9e4
zhangfei <752993248@qq.com>
2018-11-17 18:10:43 +0800

    zhangfei

--- Baocms/Lib/Action/App/UcenterAction.class.php
+++ Baocms/Lib/Action/App/UcenterAction.class.php
@@ -84,7 +84,6 @@ class UcenterAction extends CommonAction {
         $overtime=strtotime($overtime);
         //当前时间
         $time=time();
-        
         if ( !($time>$begintime && $time<$overtime)){
             $this->ajaxReturn('',"提现时间为10:00--22:00期间!");
         }
