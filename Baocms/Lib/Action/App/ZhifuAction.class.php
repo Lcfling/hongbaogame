15433f0e0758c492ca2ec8bae748de5c8bf84fbb
zhangfei <752993248@qq.com>
2018-12-03 16:44:49 +0800

    zhangfei

--- Baocms/Lib/Action/App/ZhifuAction.class.php
+++ Baocms/Lib/Action/App/ZhifuAction.class.php
@@ -123,8 +123,137 @@ class ZhifuAction extends CommonAction{
          }
 
 
+    }
+
+    public function notifyUrl(){
+
+        $payOrderId=$_POST['payOrderId'];//支付中心订单号
+        $mchId=$_POST['mchId'];//商户id
+        $appId=$_POST['appId'];//appid
+
+        $productId=$_POST['productId'];//支付方式ID
+        $mchOrderNo=$_POST['mchOrderNo'];//支付订单号
+        $amount=$_POST['amount'];//金额 以分单位
+        $channelOrderNo=$_POST['channelOrderNo'];//三方支付渠道订单号
+        $status=$_POST['status'];//支付状态,0-订单生成,1-支付中,2-支付成功,3-业务处理完成
+        $paySuccTime=$_POST['paySuccTime'];// 支付成功时间 精确到毫秒
+        $backType=$_POST['backType'];//通知类型，1-前台通知，2-后台通知
+        $sign=$_POST['sign'];//签名值
+
+        $data['payOrderId']=$payOrderId;  //1
+        $data['mchId']=$mchId;
+        $data['appId']=$appId;  //5
+        $data['productId']=$productId; //3
+        $data['mchOrderNo']=$mchOrderNo; //4
+        $data['amount']=$amount;  //2
+        $data['status']=$status;
+        $data['paySuccTime']=$paySuccTime; //6
+        $data['backType']=$backType;
+        $data['channelOrderNo']=$channelOrderNo;
+
+        $signs=$this->getSign($data);
+        if ($sign == $signs ){
+            if ($status  == 2){
+                $order=M('order');
+                $where['out_trade_no']=$mchOrderNo;
+                $save['status']=1;
+                $order->where($where)->save($save);
+                $user_info=$order->where($where)->find();
+
+                $paid=M('Paid');
+                $where['order_id']=$mchOrderNo;
+                $list=$paid->where($where)->find();
+
+                if (!$list){
+                    $info['order_id']=$mchOrderNo;
+                    $info['money']=$amount;
+                    $info['user_id']=$user_info['user_id'];
+                    $info['creatime']=time();
+                    $info['type']=1;
+                    $info['remark']='支付宝充值';
+                    $info['is_afect']=1;
+                    $paid->add($info);
+                }
+
+            }
+        }
+        echo "success";
 
     }
+
+
+
+
+    function convertUrlQuery($query)
+    {
+        $queryParts = explode('&', $query);
+        $params = array();
+        foreach ($queryParts as $param) {
+            $item = explode('=', $param);
+            $params[$item[0]] = $item[1];
+        }
+        return $params;
+    }
+
+
+
+    function getSign($Obj){
+
+        foreach ($Obj as $k => $v)
+        {
+            $Parameters[$k] = $v;
+        }
+        //签名步骤一：按字典序排序参数
+        ksort($Parameters);
+        $String =$this->formatBizQueryParaMap($Parameters, false);
+        //echo '【string1】'.$String.'</br>';
+        //签名步骤二：在string后加入KEY
+        $String = $String."&key=".'INIMNJGNPZ00ZKEIUNTQL3FBE411OWWFRRZPXXPD6C0JGMXRN3XG1EHAYWBHMX72VUMGUHV5WB3J3XJPC1ZXBTRVQV6MDORR8EN1WAPMDLYD1VFH2BSOYD04WTNMEBDN';
+        //echo "【string2】".$String."</br>";
+        //签名步骤三：MD5加密
+        $String = md5($String);
+        //echo "【string3】 ".$String."</br>";
+        //签名步骤四：所有字符转为大写
+        $result_ = strtoupper($String);
+        //echo "【result】 ".$result_."</br>";
+        return $result_;
+    }
+
+    function formatBizQueryParaMap($paraMap, $urlencode){
+        $buff = "";
+        ksort($paraMap);
+        foreach ($paraMap as $k => $v)
+        {
+            if($urlencode)
+            {
+                $v = urlencode($v);
+            }
+            //$buff .= strtolower($k) . "=" . $v . "&";
+            $buff .= $k . "=" . $v . "&";
+        }
+
+        if (strlen($buff) > 0)
+        {
+            $reqPar = substr($buff, 0, strlen($buff)-1);
+        }
+        return $reqPar;
+    }
+    function https_post_kf($url,$data)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+        if (curl_errno($curl)) {
+            return 'Errno'.curl_error($curl);
+        }
+        curl_close($curl);
+        return $result;
+    }
+
+
 
 
 
@@ -205,7 +334,7 @@ class ZhifuAction extends CommonAction{
         }
     }
 
-    function getSign($arr, $miKey) {
+    function get_Sign($arr, $miKey) {
         $str = "";
         if ($arr) {
             ksort($arr);
