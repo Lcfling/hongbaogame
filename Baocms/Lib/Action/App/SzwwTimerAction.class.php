f7842af2bb772c252364a479d62ba4060c61d0e4
lvliukun <791419612@qq.com>
2018-12-26 17:30:10 +0800

    胜者为王闲家抢包添加了冻结

--- Baocms/Lib/Action/App/SzwwTimerAction.class.php
+++ Baocms/Lib/Action/App/SzwwTimerAction.class.php
@@ -25,12 +25,16 @@ class SzwwTimerAction extends Action{
                     $hbfinishedstatus =  $this->unfreeze($v['id']);
                    // 庄家发的红包的剩余金额和冻结金额的返还
                     $this->zjmoneyback($hbinfo);
+                    //返佣扣除庄家赢得金额，结算金额入库
+                    $this->zjfypay($hbinfo);
+                    //闲家赔付入库
+                    $this->xjfypay($hbinfo);
                     //将领取的结算结果发送给房间所有人
                     $szwwsend->hbgetlistnotify($hbinfo);
+
                     //如果庄家没有领取红包自动领包并更改已领取红包缓存
                     $szwwsend->savezjkickstatus($hbinfo['id'],$hbinfo['user_id']);
-                    //返佣扣除庄家赢得金额
-                    $this->zjfypay($hbinfo);
+
                     //记录日志
                     file_put_contents('./hbstatus.log',"红包结束定时器执行状态（1 成功 0 失败）：大红包id：".$v['id']."状态：".$hbfinishedstatus.PHP_EOL,FILE_APPEND);
 
@@ -56,7 +60,6 @@ class SzwwTimerAction extends Action{
         $money = $szwwsend->zjpaymoney($hbinfo);
         //file_put_contents('./token.txt','money'.$money.PHP_EOL,FILE_APPEND);
         $users->addmoney($hbinfo['user_id'],$money[2],73,$is_afect=1,'发包解冻（胜者）',$order_id=0);
-        $users->addmoney($hbinfo['user_id'],$money[3],74,$is_afect=1,'结算（胜者）',$order_id=0);
         if($money[1] > 0){
             $users->addmoney($hbinfo['user_id'],$money[1],72,$is_afect=1,'发包返还（胜者）',$order_id=0);
         }
@@ -72,15 +75,47 @@ class SzwwTimerAction extends Action{
         $hb_id = $hb['id'];
         $uid =$hb['user_id'];
         $getmoneytotal = $szwwget->where("hb_id = $hb_id and user_id > 0")->sum('paymoney');
+        //结算扣除庄家金额
+        $users->addmoney($hb['user_id'],$getmoneytotal,74,$is_afect=1,'结算（胜者）',$order_id=0);
         //玩家盈利抽取5%
         $fymoney =$getmoneytotal * 0.05;
         if($getmoneytotal>0){
+
             //扣除闲家赢得钱
             $users->reducemoney($uid,$fymoney,81,$is_afect=1,'盈利扣除（胜者）',$order_id=0);
             //闲家的返佣
             $szwwfy->fanyong($uid,$fymoney,'szww');
 
         }
+    }
+    /**返佣扣除庄家赢得金额
+     * @param $hb 红包信息
+     */
+    private function xjfypay($hb){
+        $szwwfy =   D('Szwwfy');
+        $users =   D('Users');
+        $hb_id = $hb['id'];
+        $type = '8';
+        $remark='金额赔付（胜者）';
+        $uids = Cac()->lRange('szwwback_user_'.$hb_id,0,-1);
+        foreach ($uids as $v){
+            $money= Cac()->get('szww_paymoney_'.$hb_id.$v);
+            //闲家抢包解冻
+            $users->addmoney($v,$hb['money'],73,$is_afect=1,'抢包解冻（胜者）',$order_id=0);
+            //闲家赔付记录表
+            $users->addmoney($v,$money,$type,$is_afect=1,$remark,$order_id=0);
+            if($money>0){
+                //闲家盈利抽取5%
+                $fymoney = $money*0.05;
+                //扣除闲家赢得钱
+                $users->reducemoney($v,$fymoney,81,$is_afect=1,'盈利扣除（胜者）',$order_id=0);
+                //闲家的返佣
+                $szwwfy->fanyong($v,$fymoney,'szww');
+            }
+
+        }
+
+
     }
 
     /**庄家金额1分钟之后进行解冻 红包领取结束
